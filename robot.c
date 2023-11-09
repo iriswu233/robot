e6bbda0d636466b1082b664b6c1b96052fbad573
iriswu233 <146657644+iriswu233@users.noreply.github.com>
2023-11-09 17:03:06 +0000

    Update robot.c

--- robot.c
+++ robot.c
@@ -4,10 +4,6 @@
 #include <string.h>
 #include <stdbool.h>
 
-// use depth first search
-// east, south: positive direction
-// west, north: negative direction
-
 typedef struct Point
 {
     int x;
@@ -15,9 +11,30 @@ typedef struct Point
 } Point;
 
 Point Markers[] = {{0,7}, {7,2}, {8,8}};
-Point substituteMarker = {4,3};
-Point Blockers[] = {{1,2}, {2,4}, {4,6},{8,3}, {6,8}};
-Point substituteBlocker = {7,7};
+
+//used if one of the markers coincide with home
+Point substituteMarker = {3,1};
+
+//counter of which matker is being processed
+int counter =0;
+
+Point Blockers[] = {{2,4}, {4,6},{9,3}, {6,9}};
+
+//used if one of blockers coincide with home
+Point substituteBlocker = {5,5};
+
+//limit the range to get to each marker
+int xMin, xMax, yMin, yMax;
+
+/*
+path[] records the path of the robot
+-2: right()
+-1: unsigned
+0: left()
+other: number of times of forward() is called
+*/
+int path[100];
+int path_len = -1;
 
 bool comparePoint(Point a, Point b)
 {
@@ -29,12 +46,12 @@ bool comparePoint(Point a, Point b)
     return result;
 }
 
-
 typedef struct Robot
 {
     Point homePoint;
     Point currentPoint;
     char *direction;
+    bool pickUpMarker;
 } Robot;
 
 Robot robot;
@@ -51,6 +68,7 @@ void drawGrid()
     }
 }
 
+//returns the top left coordinate of the point in the canvas
 Point getTopLeft(Point p)
 {
     p.x = 50 + 50 * p.x;
@@ -58,89 +76,271 @@ Point getTopLeft(Point p)
     return p;
 }
 
-void drawHome(Point home)
+void drawHome()
 {
     background();
     setColour(blue);
 
-    home = getTopLeft(home);
+    Point home = getTopLeft(robot.homePoint);
     fillRect(home.x, home.y, 50, 50);
 }
 
-void drawRobot(Point pos, const char *direction)
+void drawRobot()
 {
     foreground();
     setColour(green);
 
-    if (strcmp(direction, "north") == 0)
+    int x = robot.currentPoint.x;
+    int y = robot.currentPoint.y;
+
+    if (strcmp(robot.direction, "north") == 0)
     {
-        int xCoords[] = {50 + pos.x * 50, 75 + pos.x * 50, 100 + pos.x * 50};
-        int yCoords[] = {100 + pos.y * 50, 50 + pos.y * 50, 100 + pos.y * 50};
+        int xCoords[] = {50 + x * 50, 75 + x * 50, 100 + x * 50};
+        int yCoords[] = {100 + y * 50, 50 + y * 50, 100 + y * 50};
         fillPolygon(3, xCoords, yCoords);
     }
-    else if (strcmp(direction, "east") == 0)
+    else if (strcmp(robot.direction, "east") == 0)
     {
-        int xCoords[] = {50 + pos.x * 50, 50 + pos.x * 50, 100 + pos.x * 50};
-        int yCoords[] = {50 + pos.y * 50, 100 + pos.y * 50, 75 + pos.y * 50};
+        int xCoords[] = {50 + x * 50, 50 + x * 50, 100 + x * 50};
+        int yCoords[] = {50 + y * 50, 100 + y * 50, 75 + y * 50};
         fillPolygon(3, xCoords, yCoords);
     }
-    else if (strcmp(direction, "south") == 0)
+    else if (strcmp(robot.direction, "south") == 0)
     {
-        int xCoords[] = {50 + pos.x * 50, 100 + pos.x * 50, 75 + pos.x * 50};
-        int yCoords[] = {50 + pos.y * 50, 50 + pos.y * 50, 100 + pos.y * 50};
+        int xCoords[] = {50 + x * 50, 100 + x * 50, 75 + x * 50};
+        int yCoords[] = {50 + y * 50, 50 + y * 50, 100 + y * 50};
         fillPolygon(3, xCoords, yCoords);
     }
-    else if (strcmp(direction, "west") == 0)
+    else if (strcmp(robot.direction, "west") == 0)
     {
-        int xCoords[] = {50 + pos.x * 50, 100 + pos.x * 50, 100 + pos.x * 50};
-        int yCoords[] = {75 + pos.y * 50, 50 + pos.y * 50, 100 + pos.y * 50};
+        int xCoords[] = {50 + x * 50, 100 + x * 50, 100 + x * 50};
+        int yCoords[] = {75 + y * 50, 50 + y * 50, 100 + y * 50};
         fillPolygon(3, xCoords, yCoords);
     }
+    
 }
 
-void drawMarker(Point marker)
+void drawMarker()
 {
-    foreground();
+    background();
     setColour(gray);
 
-    marker = getTopLeft(marker);
-    fillRect(marker.x, marker.y, 50, 50);
+    for(int i=0; i<3; i++)
+    {
+        Point marker = getTopLeft(Markers[i]);
+        fillRect(marker.x, marker.y, 50, 50);
+
+    }
+
 }
 
-void drawBlocker(Point blocker)
+void drawBlocker()
 {
     background();
     setColour(black);
 
-    blocker = getTopLeft(blocker);
-    fillRect(blocker.x, blocker.y, 50,50);
+    for(int i=0; i<4; i++)
+    {
+        Point blocker = getTopLeft(Blockers[i]);
+        fillRect(blocker.x, blocker.y, 50,50);
+    }
+}
+
+void drawBackground()
+{
+    drawGrid();
+    drawBlocker();
+    drawMarker();
+    drawHome();
 }
 
 void left()
 {
+    foreground();
+    clear();
+    if (strcmp(robot.direction, "north") == 0)
+    {
+        robot.direction = "west";
+        drawRobot();
+    }
+    else if(strcmp(robot.direction, "west") == 0)
+    {
+        robot.direction = "south";
+        drawRobot();
+    }
+    else if(strcmp(robot.direction, "south") == 0)
+    {
+        robot.direction = "east";
+        drawRobot();
+    }
+    else
+    {
+        robot.direction = "north";
+        drawRobot();
+    }
+}
+
+void right()
+{
+    foreground();
+    clear();
+    if (strcmp(robot.direction, "north") == 0)
+    {
+        robot.direction = "east";
+        drawRobot();
+    }
+    else if(strcmp(robot.direction, "west") == 0)
+    {
+        robot.direction = "north";
+        drawRobot();
+    }
+    else if(strcmp(robot.direction, "south") == 0)
+    {
+        robot.direction = "west";
+        drawRobot();
+    }
+    else
+    {
+        robot.direction = "south";
+        drawRobot();
+    }
+}
+
+//returns the expected next point if forward() is called
+// east, south: positive direction
+// west, north: negative direction
+Point nextPosition()
+{
+    Point result = robot.currentPoint;
+    if (strcmp(robot.direction, "north") == 0)
+    {
+        result.y -=1;
+    }
+    else if(strcmp(robot.direction, "south") == 0)
+    {
+        result.y +=1;
+    }
+    else if(strcmp(robot.direction, "west") == 0)
+    {
+        result.x -= 1;
+    }
+    else
+    {
+        result.x += 1;
+    }
+    return result;
+
+}
+
+bool canMoveForward()
+{
+    Point next = nextPosition();
+
+    //the robot should not exceed the range
+    if ((next.x <xMin) || (next.x > xMax))
+    {
+        return false;
+    }
+    else if ((next.y <yMin) || (next.y > yMax))
+    {
+        return false;
+    }
+
+    for (int i=0; i<4; i++)
+    {
+        if(comparePoint(Blockers[i], next) == true)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+bool atMarker(Point marker)
+{
+    return comparePoint(robot.currentPoint, marker);
+}
+
+bool atHome()
+{
+    return comparePoint(robot.homePoint, robot.currentPoint);
+}
+
+void forward()
+{
     
+    foreground();
+    clear();
+    robot.currentPoint = nextPosition();
+    if(robot.pickUpMarker)
+    {
+        Markers[counter] = robot.currentPoint;
+    }
+    drawRobot();
+    
+}
+
+void returnHome()
+{
+    //turn around
+    left();
+    sleep(500);
+    left();
+    sleep(500);
+
+    for(int i=path_len; i >= 0; i--)
+    {
+        if(atHome())
+        {
+            return;
+        }
+
+        //opposite direction of the recorded path
+        if (path[i] == 0)
+        {
+            right();
+            sleep(500);
+        }
+        else if (path[i] == -2)
+        {
+            left();
+            sleep(500);
+        }
+        
+        else
+        {
+            for(int j=0; j< path[i]; j++)
+            {
+                forward();
+
+                background();
+                clear();
+                drawBackground();
+                sleep(500);
+            }
+        }
+    }
 }
 
 int main(int argc, char **argv)
 {
     setWindowSize(600, 600);
-    drawGrid();
 
     robot.homePoint.x = 6;
     robot.homePoint.y = 5;
-    robot.currentPoint = robot.homePoint;
     robot.direction = "north";
+    robot.pickUpMarker = false;
 
     if (argc == 4)
     {
         robot.homePoint.x = atoi(argv[1]);
         robot.homePoint.y = atoi(argv[2]);
-        robot.currentPoint = robot.homePoint;
         robot.direction = argv[3];
     }
+    robot.currentPoint = robot.homePoint;
 
-    drawHome(robot.homePoint);
-    drawRobot(robot.currentPoint, robot.direction);
+    drawRobot();
     
     for (int i=0; i<3; i++)
     {
@@ -148,23 +348,155 @@ int main(int argc, char **argv)
         {
             Markers[i] = substituteMarker;
         }
-        drawMarker(Markers[i]);
     }
 
-    for (int i=0; i<5; i++)
+    for (int i=0; i<4; i++)
     {
         if(comparePoint(Blockers[i], robot.homePoint) == true)
         {
             Blockers[i] = substituteBlocker;
         }
-        drawBlocker(Blockers[i]);
     }
 
-    for(int counter=0; counter<3; counter++)
-    {
-        int x_path = Markers[counter].x - robot.homePoint.x;
-        int y_path = Markers[counter].y - robot.homePoint.y;
+    drawBackground();
 
+    //process each marker
+    for(counter=0; counter <3; counter++)
+    {
+        robot.pickUpMarker = false;
+
+        //home and the marker are two diagonal corners of the rectangle range
+        if(robot.currentPoint.x < Markers[counter].x)
+        {
+            xMin = robot.currentPoint.x;
+            xMax = Markers[counter].x;
+        }
+        else
+        {
+            xMin = Markers[counter].x;
+            xMax = robot.currentPoint.x;
+        }
+
+        //let the robot facing the correct vertical direction to the marker
+        if(robot.currentPoint.y < Markers[counter].y)
+        {
+            yMin = robot.currentPoint.y;
+            yMax = Markers[counter].y;
+            robot.direction = "south";
+            foreground();
+            clear();
+            drawRobot();
+        }
+        else
+        {
+            yMin = Markers[counter].y;
+            yMax = robot.currentPoint.y;
+            robot.direction = "north";
+            foreground();
+            clear();
+            drawRobot();
+        }
+
+        for(int i=0; i< 100; i++)
+        {
+            path[i] = -1;
+        }
+        path_len = -1;
+
+        while(!atMarker(Markers[counter]))
+        {
+
+            sleep(500);
+            
+            /*
+            Both the robot and the marker are at the corner of the rectangle range.
+
+            If the robot always moves next to the wall of the range, 
+            it will definitely get to the marker.
+
+            Let the robot's right hand side always be the wall (or the blocker).
+            */
+
+            //check if can move in right direction
+            right();
+
+            if(canMoveForward())
+            {
+                path_len += 1;
+                path[path_len] = -2;
+
+                forward();
+                path_len += 1;
+                path[path_len] = 1;
+            }
+            else
+            {
+                //return to original directon
+                left();
+
+                //check if can move forward
+                if(canMoveForward())
+                {
+                    forward();
+                    switch(path[path_len])
+                    {
+                    //open a new path
+                    case -1:
+                        path_len += 1;
+                        path[path_len] = 1;
+                        break;
+                    
+                    //the last action is left()
+                    //record a new distance in a new int element
+                    case 0:
+                        path_len += 1;
+                        path[path_len] = 1;
+                        break;
+
+                    //the last action is right()
+                    //record a new distance in a new int element
+                    case -2:
+                        path_len += 1;
+                        path[path_len] = 1;
+                        break;
+
+                    //the last action is forward()
+                    //increment the distance
+                    default:
+                        path[path_len] += 1;
+                        break;
+                    }
+                }
+                else
+                {
+                    //check if can move in left direction
+                    left();
+                    path_len += 1;
+                    path[path_len] = 0;
+
+                    if(canMoveForward())
+                    {
+                        forward();
+                        path_len += 1;
+                        path[path_len] = 1;
+                    }
+                    
+                    //all three directions are unavailable, turn around
+                    else
+                    {
+                        left();
+                        path_len += 1;
+                        path[path_len] = 0;
+                    }
+
+                }
+            }
+            
+        }
+        robot.pickUpMarker = true;
+
+        sleep(500);
+        returnHome();
 
     }
 
